94aac6c460aaa952140040ef734c1254aacc08c7
Wangtutu6166 <58415489+Wangtutu6166@users.noreply.github.com>
2019-12-05 17:23:04 +0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,22 +1,25 @@
 window.onload = function(){
 	var ibtn = document.getElementById("search-b");
 	ibtn.onclick = function(){
-		var itext = document.getElementById("search-t").value;
-		if(itext == ""){
+		var itext = document.getElementById("search-t");
+		if(itext.value == ""){
 			alert("请输入要搜索的关键字");
 		}else{
-			window.location.href = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=" + itext;
+			window.open ("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=" + itext.value);
 		}
 	}
 }
-document.onkeydown = function(event){
-	var keydown = event || window.event || arguments.callee.caller.arguments[0];
-	if (keydown && keydown.keyCode == 13){
-		var itext = document.getElementById("search-t").value;
-		if(itext == ""){
-			alert("请输入要搜索的关键字");
-		}else{
-			window.location.href = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=" + itext;
+function Mousefoucs(){
+	document.onkeydown = function(event){
+		var keydown = event || window.event || arguments.callee.caller.arguments[0];
+		if (keydown && keydown.keyCode == 13){
+			var itext = document.getElementById("search-t");
+			if(itext.value == ""){
+				alert("请输入要搜索的关键字");
+			}else{
+				window.open ("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=" + itext.value);
+			}
+			return false;
 		}
 	}
 }
