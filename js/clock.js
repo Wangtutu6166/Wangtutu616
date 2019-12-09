d6e0922be50ce830ccc34a5d85ee299d1efb04fc
Wangtutu6166 <58415489+Wangtutu6166@users.noreply.github.com>
2019-12-09 14:51:37 +0800

    Add files via upload

---
+++ js/clock.js
@@ -0,0 +1,17 @@
+setInterval(function(){
+	var date = new Date();
+	updateTime(date);
+},1000);
+function updateTime(date){
+	var timeDiv = document.getElementById("clock_2");
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	if(hours<10){
+		hours = "0" + hours;
+	}
+	if(minutes<10){
+		minutes = "0" + minutes;
+	}
+	var time = hours + ":" + minutes;
+	timeDiv.innerHTML = time;
+}
---
+++ js/tantiaorecerh.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$(".search-box").animate({"top":360},1000,"tantiao");
+	$("#clock_2").addClass('animated bounceIn');
+});
